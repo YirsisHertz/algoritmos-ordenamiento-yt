d2ee6dca770441ce1c464893b922f8076b7de27b
YirsisHertz <yirsishertz02@gmail.com>
2021-06-10 17:54:42 -0500

    Dinamic Values

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void insertionSort(int valores[], int n){
   int i, j, aux;
+  system("clear");
 
   for(i = 1; i < n; i++){
     j = i;
@@ -17,20 +19,32 @@ void insertionSort(int valores[], int n){
 
 int main(){
 
-  int valores[] = {31, 25, 43, 12, 8, 67, 14};
+  int valores[5];
+
+  for(int i = 0; i < 5; i++){
+    scanf("%i", &valores[i]);
+  }
+
   int n = sizeof valores / sizeof valores[0];
+
+  printf("Desordenado: ");
 
   for(int i = 0; i < n; i++){
     printf("%i, ", valores[i]);
+    /* cprintf("Desordenado: "); */
   }
 
   insertionSort(valores, n);
 
   printf("\n");
+  printf("Ordenado: ");
+  /* textcolor(2); */
 
 for(int i = 0; i < n; i++){
-    printf("%i, ", valores[i]);
+    printf("%i, ", valores[i]); 
   }
+
+ printf("\n");
 
   return 0;
 }
